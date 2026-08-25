c15983e1a18e7071811544a79bc9a1f389a1657b
malescript34 <malescript34@gmail.com>
2026-08-25 11:22:43 +0530

    Create a new listing
    Not able to enter data inside the database

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ async function main(){
 }
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({ extended: true }));
 app.get("/",(req,res) => {
     res.send("I am root");
 });
@@ -26,10 +27,21 @@ app.get("/listings",async (req,res)=>{
     res.render("listings/index.ejs",{allListings});
 });
 
+app.get("/listings/new" ,(req,res)=>{
+    res.render("listings/new.ejs")
+})
+
 app.get("/listings/:id",async (req,res)=>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs",{listing});
+});
+
+app.post("/listings",async(req,res)=>{
+    // let{title,decription,image,price,location,country} = req.body;
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
 });
 // app.get("/testListing",async (req,res)=>{
 //     let sampleListing = new Listing({
