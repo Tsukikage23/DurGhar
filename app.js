93bb03ac2b908b3a59329461cbcde8918442d53b
malescript34 <malescript34@gmail.com>
2026-08-25 10:05:16 +0530

    index route made
    Learnt about ejs

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js")
+const path = require("path");
 const mongo_url = "mongodb://127.0.0.1:27017/durghar";
-
 main()
     .then(() => {
         console.log("Connected to db");
@@ -15,22 +15,28 @@ main()
 async function main(){
     await mongoose.connect(mongo_url);
 }
-
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
 app.get("/",(req,res) => {
     res.send("I am root");
 });
-app.get("/testListing",async (req,res)=>{
-    let sampleListing = new Listing({
-        title: "Me new home",
-        description: "by the beach",
-        price: 1200,
-        location: "kota,rajasthan",
-        country: "India",
-    })
-    await sampleListing.save();
-    console.log("sample saved");
-    res.send("successful testing");
+// index route
+app.get("/listings",async (req,res)=>{
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs",{allListings});
 });
+// app.get("/testListing",async (req,res)=>{
+//     let sampleListing = new Listing({
+//         title: "Me new home",
+//         description: "by the beach",
+//         price: 1200,
+//         location: "kota,rajasthan",
+//         country: "India",
+//     })
+//     await sampleListing.save();
+//     console.log("sample saved");
+//     res.send("successful testing");
+// });
 app.listen(8080,() =>{
     console.log("Server is listening on port 8080");
 });
