aaea5461a1e82186f67c8155fe70b91fa73f43e0
malescript34 <malescript34@gmail.com>
2026-09-03 19:16:45 +0530

    Delete Route

--- app.js
+++ app.js
@@ -58,6 +58,14 @@ app.put("/listings/:id" ,async (req,res)=>{
     res.redirect(`/listings/${id}`);
 });
 
+
+app.delete("/listings/:id" ,async (req,res)=>{
+    let {id} = req.params;
+    let Deleted = await Listing.findByIdAndDelete(id);
+    console.log(Deleted);
+    res.redirect("/listings");
+})
+
 // app.get("/testListing",async (req,res)=>{
 //     let sampleListing = new Listing({
 //         title: "Me new home",
